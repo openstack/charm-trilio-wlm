f9b820db89aee6144d7b5a97841455c0b048ec63
James Page <james.page@ubuntu.com>
2020-03-06 10:58:06 +0000

    Misc tidy

--- unit_tests/test_lib_charm_openstack_openvswitch_odl.py
+++ unit_tests/test_lib_charm_openstack_openvswitch_odl.py
@@ -12,12 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import absolute_import
-from __future__ import print_function
-
-import unittest
 
 import mock
+import unittest
 
 import charm.openstack.sdn_charm as sdn_charm
 
